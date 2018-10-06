7971fe92b454b6a8500f2c93cf93d9a0ea6965e9
msf7415003@teilar.gr <msf7415003@teilar.gr>
2018-10-06 18:00:40 +0300

    Security enabled + Bcrypt

--- src/main/java/com/example/mobileapp/Services/Impl/UserServiceImplementation.java
+++ src/main/java/com/example/mobileapp/Services/Impl/UserServiceImplementation.java
@@ -4,8 +4,10 @@ import com.example.mobileapp.Entity.UserEntity;
 import com.example.mobileapp.Repositories.UserRepository;
 import com.example.mobileapp.Services.UserService;
 import com.example.mobileapp.Sharred.dto.UserDto;
+import com.example.mobileapp.Sharred.dto.Utils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,13 +16,23 @@ public class UserServiceImplementation implements UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    Utils utils;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Override
     public UserDto createUser(UserDto user) {
 
+        if(userRepository.findUserEntityByEmail(user.getEmail()) != null) throw new RuntimeException("Record already exists");
+
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(user,userEntity);
-        userEntity.setEncryptedPassword("test");
-        userEntity.setUserId("testUserID");
+
+        String publicUserId = utils.generateUserID(30);
+        userEntity.setUserId(publicUserId);
+        userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         UserEntity storedUserDetails = userRepository.save(userEntity);
         UserDto returnValue = new UserDto();
         BeanUtils.copyProperties(storedUserDetails,returnValue);
