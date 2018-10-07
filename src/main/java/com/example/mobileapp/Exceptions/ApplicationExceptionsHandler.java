984b5eb96aa34d9c4d9dfd0261f96e6d91922f82
msf7415003@teilar.gr <msf7415003@teilar.gr>
2018-10-07 22:33:26 +0300

    Custom Error Message Object created

---
+++ src/main/java/com/example/mobileapp/Exceptions/ApplicationExceptionsHandler.java
@@ -0,0 +1,22 @@
+package com.example.mobileapp.Exceptions;
+
+import com.example.mobileapp.Model.Response.ErrorMessage;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class ApplicationExceptionsHandler {
+    @ExceptionHandler(value = {UserServiceException.class})
+    public ResponseEntity<Object> handleUserServiceException(UserServiceException ex,
+                                                             WebRequest request){
+
+        ErrorMessage errorMessage = new ErrorMessage(new Date(),ex.getMessage());
+        return new ResponseEntity<>(errorMessage,new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
